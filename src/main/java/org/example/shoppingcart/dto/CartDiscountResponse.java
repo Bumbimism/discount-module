77fbeddb09728a46e4036bbf73ed50965c81cc7b
Bumbimism <aqualiux.nittha@gmail.com>
2024-05-16 22:39:47 +0700

    add constructure

--- src/main/java/org/example/shoppingcart/dto/CartDiscountResponse.java
+++ src/main/java/org/example/shoppingcart/dto/CartDiscountResponse.java
@@ -5,4 +5,8 @@ import lombok.Data;
 @Data
 public class CartDiscountResponse {
     private double totalPrice;
+
+    public CartDiscountResponse(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
